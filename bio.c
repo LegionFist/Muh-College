cbfab540866936ce21386e976a74155bba6d678f
unknown <dzakialv@gmail.com>
2020-09-16 12:07:27 +0700

    hadehhh

---
+++ bio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+	printf("=============== PROGRAM BIODATA SAYA ===============\n");
+	printf("\nNama	: Mochammad Dzaki Al Vriano\n");
+	printf("TTL	: Surabaya, 22 Juli 2001\n");
+	printf("Alamat	: TPI \n");
+	printf("NPM	: 19081010138\n");
+	printf("Jurusan	: Teknik Informatika\n");
+	printf("Angkatan: 2019\n");
+	printf("\n=================== TERIMAKASIH ===================");
+}
